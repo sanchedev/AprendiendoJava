a510c26d62deba6ef103384a6fd2ca263aa87415
Sanchedev <sanchedev@outlook.com>
2026-04-01 20:09:14 -0500

    creando el proyecto}

---
+++ src/clases/S01ImprimirEnConsola.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author sanchedev
+ */
+public class S01ImprimirEnConsola {
+    public static void main(String[] args) {
+    }
+}
